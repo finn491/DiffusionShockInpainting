25442507cb5456c0fb6f5f431a8a658807c12a0a
Finn Sherry <f.m.sherry@tue.nl>
2024-03-04 09:26:27 +0100

    copy visualisation tools from EikIVP.

---
+++ dsfilter/R2/utils.py
@@ -0,0 +1,287 @@
+"""
+    utils
+    =====
+
+    Provides miscellaneous computational utilities that can be used on R^2.
+"""
+
+import numpy as np
+import taichi as ti
+
+# Safe Indexing
+
+@ti.func
+def sanitize_index(
+    index: ti.types.vector(2, ti.i32),
+    input: ti.template()
+) -> ti.types.vector(2, ti.i32):
+    """
+    @taichi.func
+    
+    Make sure the `index` is inside the shape of `input`. Adapted from Gijs.
+
+    Args:
+        `index`: ti.types.vector(n=2, dtype=ti.i32) index.
+        `input`: ti.field in which we want to index.
+
+    Returns:
+        ti.types.vector(n=2, dtype=ti.i32) of index that is within `input`.
+    """
+    shape = ti.Vector(ti.static(input.shape), dt=ti.i32)
+    return ti.Vector([
+        ti.math.clamp(index[0], 0, shape[0] - 1),
+        ti.math.clamp(index[1], 0, shape[1] - 1),
+    ], dt=ti.i32)
+
+# Distance Map
+
+def get_boundary_conditions(source_point):
+    """
+    Determine the boundary conditions from `source_point`, giving the boundary
+    points and boundary values as TaiChi objects.
+    """
+    i_0, j_0 = source_point
+    boundarypoints_np = np.array([[i_0 + 1, j_0 + 1]], dtype=int) # Account for padding.
+    boundaryvalues_np = np.array([0.], dtype=float)
+    boundarypoints = ti.Vector.field(n=2, dtype=ti.i32, shape=1)
+    boundarypoints.from_numpy(boundarypoints_np)
+    boundaryvalues = ti.field(shape=1, dtype=ti.f32)
+    boundaryvalues.from_numpy(boundaryvalues_np)
+    return boundarypoints, boundaryvalues
+
+@ti.kernel
+def field_abs_max(
+    scalar_field: ti.template()
+) -> ti.f32:
+    """
+    @taichi.kernel
+
+    Find the largest absolute value in `scalar_field`.
+
+    Args:
+        static: ti.field(dtype=[float], shape=shape) of 2D scalar field.
+
+    Returns:
+        Largest absolute value in `scalar_field`.
+    """
+    value = ti.abs(scalar_field[0, 0])
+    for I in ti.grouped(scalar_field):
+        value = ti.atomic_max(value, ti.abs(scalar_field[I]))
+    return value
+
+def check_convergence(dW_dt, tol=1e-3, target_point=None):
+    """
+    Check whether the IVP method has converged by comparing the Hamiltonian
+    `dW_dt` to tolerance `tol`. If `target_point` is provided, only check
+    convergence at `target_point`; otherwise check throughout the domain.
+    """
+    is_converged = False
+    if target_point is None:
+        error = field_abs_max(dW_dt)
+        print(error)
+        is_converged = error < tol
+    else:
+        error = ti.abs(dW_dt[target_point])
+        print(error)
+        is_converged = error < tol
+    return is_converged
+
+# Coordinate Transforms
+
+def coordinate_real_to_array(x, y, x_min, y_min, dxy):
+    """
+    Compute the array indices (I, J) of the point defined by real coordinates 
+    (`x`, `y`). Can broadcast over entire arrays of real coordinates.
+
+    Args:
+        `x`: x-coordinate of the point.
+        `y`: y-coordinate of the point.
+        `x_min`: minimum value of x-coordinates in rectangular domain.
+        `y_min`: minimum value of y-coordinates in rectangular domain.
+        `dxy`: spatial resolution, which is equal in the x- and y-directions,
+          taking values greater than 0.
+    """
+    I = np.rint((x - x_min) / dxy).astype(int)
+    J = np.rint((y - y_min) / dxy).astype(int)
+    return I, J
+
+
+def coordinate_array_to_real(I, J, x_min, y_min, dxy):
+    """
+    Compute the real coordinates (x, y) of the point defined by array indices 
+    (`I`, `J`). Can broadcast over entire arrays of array indices.
+
+    Args:
+        `I`: I index of the point.
+        `J`: J index of the point.
+        `x_min`: minimum value of x-coordinates in rectangular domain.
+        `y_min`: minimum value of y-coordinates in rectangular domain.
+        `dxy`: spatial resolution, which is equal in the x- and y-directions,
+          taking values greater than 0.
+    """
+    x = x_min + I * dxy
+    y = y_min + J * dxy
+    return x, y
+
+def align_to_real_axis_point(point, shape):
+    """
+    Express `point`, given in indices with respect to standard array convention,
+    in terms of indices with respect to arrays aligned with real axes (see Notes
+    for more explanation). Here, `shape` gives the shape of the array in which
+    we index _after_ aligning with real axes.
+
+    Args:
+        `point`: Tuple[int, int] describing point with respect to standard array
+          indexing convention.
+        `shape`: shape of array, aligned to real axes, in which we want to
+          index. Note that `0 <= point[0] <= shape[1] - 1` and 
+          `0 <= point[1] <= shape[0] - 1`.
+
+    Notes:
+        Alignment is achieved by first flipping and subsequently transposing the
+        array.
+            
+    ===================== DRAWING DOES NOT WORK IN HELP ========================    
+        
+               standard                  real axes aligned
+            I ^ ------                    I x ------
+            | | |    |        =>          | | |    |
+            v y ------                    v v ------
+                 x ->                          y ->
+                 J ->                          J ->  
+    """
+    return point[1], shape[1] - 1 - point[0]
+
+def align_to_real_axis_scalar_field(field):
+    """
+    Align `field`, given in indices with respect to standard array convention, 
+    with real axes (see Notes for more explanation).
+
+    Args:
+        `field`: np.ndarray of scalar field given with respect to standard array
+          convention.
+
+    Notes:
+        Alignment is achieved by first flipping and subsequently transposing the
+        array.
+            
+    ===================== DRAWING DOES NOT WORK IN HELP ========================    
+        
+               standard                  real axes aligned
+            I ^ ------                    I x ------
+            | | |    |        =>          | | |    |
+            v y ------                    v v ------
+                 x ->                          y ->
+                 J ->                          J ->  
+    """
+    field_flipped = np.flip(field, axis=0)
+    field_aligned = np.transpose(field_flipped, axes=(1, 0))
+    return field_aligned
+
+def align_to_real_axis_vector_field(vector_field):
+    """
+    Align `vector_field`, given in indices with respect to standard array 
+    convention, with real axes (see Notes for more explanation).
+    
+    Args:
+        `vector_field`: np.ndarray of vector field given with respect to 
+          standard array convention.
+
+    Notes:
+        Alignment is achieved by first flipping and subsequently transposing the
+        array.
+            
+    ===================== DRAWING DOES NOT WORK IN HELP ========================    
+        
+               standard                  real axes aligned
+            I ^ ------                    I x ------
+            | | |    |        =>          | | |    |
+            v y ------                    v v ------
+                 x ->                          y ->
+                 J ->                          J ->  
+    """
+    vector_field_flipped = np.flip(vector_field, axis=0)
+    vector_field_aligned = np.transpose(vector_field_flipped, axes=(1, 0, 2))
+    return vector_field_aligned
+
+def align_to_standard_array_axis_point(point, shape):
+    """
+    Express `point`, given in indices with respect to arrays aligned with real 
+    axes, in terms of indices with respect to standard array convention, (see 
+    Notes for more explanation). Here, `shape` gives the shape of the array in 
+    which we index _after_ aligning with standard array convention.
+
+    Args:
+        `point`: Tuple[int, int] describing point with respect to arrays aligned
+          with real axes.
+        `shape`: shape of array, with respect to standard array convention, in 
+          which we want to index. Note that `0 <= point[0] <= shape[1] - 1` and 
+          `0 <= point[1] <= shape[0] - 1`.
+
+    Notes:
+        Alignment is achieved by first transposing and subsequently flipping the
+        array.
+            
+    ===================== DRAWING DOES NOT WORK IN HELP ========================    
+        
+           real axes aligned                 standard
+            I x ------                    I ^ ------
+            | | |    |        =>          | | |    |
+            v v ------                    v y ------
+                 y ->                          x ->
+                 J ->                          J ->  
+    """
+    return point[1], shape[1] - 1 - point[0]
+
+def align_to_standard_array_axis_scalar_field(field):
+    """
+    Align `field`, given in indices with respect to arrays aligned with real
+    axes, with respect to standard array convention (see Notes for more 
+    explanation).
+
+    Args:
+        `field`: np.ndarray of scalar field given in indices with respect to
+          arrays aligned with real axes.
+
+    Notes:
+        Alignment is achieved by first flipping and subsequently transposing the
+        array.
+            
+    ===================== DRAWING DOES NOT WORK IN HELP ========================    
+        
+           real axes aligned                 standard
+            I x ------                    I ^ ------
+            | | |    |        =>          | | |    |
+            v v ------                    v y ------
+                 y ->                          x ->
+                 J ->                          J ->  
+    """
+    field_transposed = np.transpose(field, axes=(1, 0))
+    field_aligned = np.flip(field_transposed, axis=0)
+    return field_aligned
+
+def align_to_standard_array_axis_vector_field(vector_field):
+    """
+    Align `vector_field`, given in with respect to standard array convention, 
+    with real axes (see Notes for more explanation).
+
+    Args:
+        `vector_field`: np.ndarray of vector field given in indices with respect
+          to arrays aligned with real axes.
+
+    Notes:
+        Alignment is achieved by first flipping and subsequently transposing the
+        array.
+            
+    ===================== DRAWING DOES NOT WORK IN HELP ========================    
+        
+           real axes aligned                 standard
+            I x ------                    I ^ ------
+            | | |    |        =>          | | |    |
+            v v ------                    v y ------
+                 y ->                          x ->
+                 J ->                          J ->  
+    """
+    vector_field_transposed = np.transpose(vector_field, axes=(1, 0, 2))
+    vector_field_aligned = np.flip(vector_field_transposed, axis=0)
+    return vector_field_aligned
--- dsfilter/visualisations.py
+++ dsfilter/visualisations.py
@@ -2,20 +2,27 @@
     visualisations
     ==============
     
-    Provides methods to visualise 2D images using matplotlib.
+    Provides methods to visualise 2D and 3D images using matplotlib.
 """
 
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+from dsfilter.R2.utils import (
+    align_to_standard_array_axis_scalar_field,
+    align_to_standard_array_axis_vector_field
+)
+# from dsfilter.SO3.utils import align_to_standard_array_axis_scalar_field as align_to_standard_array_axis_scalar_field_SO3
 
 
 def convert_array_to_image(image_array):
     """Convert numpy array `image_array` to a grayscale PIL Image object."""
-    if image_array.dtype == "uint8":
-        image = Image.fromarray(image_array, mode="L")
+    image_array_aligned = align_to_standard_array_axis_scalar_field(image_array)
+    
+    if image_array_aligned.dtype == "uint8":
+        image = Image.fromarray(image_array_aligned, mode="L")
     else:
-        image = Image.fromarray((image_array * 255).astype("uint8"), mode="L")
+        image = Image.fromarray((image_array_aligned * 255).astype("uint8"), mode="L")
     return image
 
 def view_image_array(image_array):
@@ -42,33 +49,80 @@ def view_image_arrays_side_by_side(image_array_list):
     return image_list, fig, ax
 
 def plot_image_array(image_array, x_min, x_max, y_min, y_max, cmap="gray", figsize=(10, 10), fig=None, ax=None):
+    """Plot `image_array` as a heatmap."""
     if fig is None and ax is None:
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=figsize)
         ax.set_xlabel("$x$")
         ax.set_ylabel("$y$")
         ax.set_xlim(x_min, x_max)
         ax.set_ylim(y_min, y_max)
-    ax.imshow(image_array, cmap=cmap, extent=(x_min, x_max, y_min, y_max))
+
+    image_array_aligned = align_to_standard_array_axis_scalar_field(image_array)
+
+    ax.imshow(image_array_aligned, cmap=cmap, extent=(x_min, x_max, y_min, y_max))
     return fig, ax
+
+# def plot_image_array_SO3(image_array, α_min, α_max, β_min, β_max, cmap="gray", figsize=(10, 10), fig=None, ax=None):
+#     """Plot `image_array` as a heatmap."""
+#     if fig is None and ax is None:
+#         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=figsize)
+#         ax.set_xlabel("$β$")
+#         ax.set_ylabel("$α$")
+#         ax.set_xlim(β_max, β_min)
+#         ax.set_ylim(α_min, α_max)
+
+#     image_array_aligned = align_to_standard_array_axis_scalar_field_SO3(np.expand_dims(image_array, axis=2)).squeeze(-1)
+
+#     ax.imshow(image_array_aligned, cmap=cmap, extent=(β_max, β_min, α_min, α_max))
+#     return fig, ax
 
 def plot_contour(distance, xs, ys, levels=None, linestyles=None, figsize=(12, 10), fig=None, ax=None, x_min=None, 
                  x_max=None, y_min=None, y_max=None):
+    """Plot the contours of the two-dimensional array `distance`."""
     if fig is None and ax is None:
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=figsize)
         ax.set_xlabel("$x$")
         ax.set_ylabel("$y$")
         x_min = overwrite_default(x_min, xs[0, 0])
         x_max = overwrite_default(x_max, xs[-1, -1])
-        y_min = overwrite_default(y_min, ys[-1, -1])
-        y_max = overwrite_default(y_max, ys[0, 0])
+        y_min = overwrite_default(y_min, ys[0, 0])
+        y_max = overwrite_default(y_max, ys[-1, -1])
         ax.set_xlim(x_min, x_max)
         ax.set_ylim(y_min, y_max)
-    contour = ax.contour(xs, ys, distance, levels=levels, linestyles=linestyles)
+
+    xs_aligned = align_to_standard_array_axis_scalar_field(xs)
+    ys_aligned = align_to_standard_array_axis_scalar_field(ys)
+    distance_aligned = align_to_standard_array_axis_scalar_field(distance)
+    
+    contour = ax.contour(xs_aligned, ys_aligned, distance_aligned, levels=levels, linestyles=linestyles)
     return fig, ax, contour
 
-def plot_isosurface(verts, faces, x_min, x_max, y_min, y_max, θ_min, θ_max, dxy, dθ, alpha=0.5, label=None, figsize=(10, 10), fig=None, ax=None):
+# def plot_contour_SO3(distance, αs, βs, levels=None, linestyles=None, figsize=(12, 10), fig=None, ax=None, α_min=None,
+#                      α_max=None, β_min=None, β_max=None):
+#     """Plot the contours of the two-dimensional array `distance`."""
+#     if fig is None and ax is None:
+#         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=figsize)
+#         ax.set_xlabel("$β$")
+#         ax.set_ylabel("$α$")
+#         α_min = overwrite_default(α_min, αs[0, 0])
+#         α_max = overwrite_default(α_max, αs[-1, -1])
+#         β_min = overwrite_default(β_min, βs[0, 0])
+#         β_max = overwrite_default(β_max, βs[-1, -1])
+#         ax.set_xlim(β_max, β_min)
+#         ax.set_ylim(α_min, α_max)
+
+#     αs_aligned = align_to_standard_array_axis_scalar_field_SO3(αs)
+#     βs_aligned = align_to_standard_array_axis_scalar_field_SO3(βs)
+#     distance_aligned = align_to_standard_array_axis_scalar_field_SO3(np.expand_dims(distance, axis=2)).squeeze(-1)
+    
+#     contour = ax.contour(βs_aligned, αs_aligned, distance_aligned, levels=levels, linestyles=linestyles)
+#     return fig, ax, contour
+
+def plot_isosurface(verts, faces, x_min, x_max, y_min, y_max, θ_min, θ_max, dxy, dθ, alpha=0.5, label=None,
+                    figsize=(10, 10), fig=None, ax=None):
+    """Plot the isosurface given by `verts` and `faces`."""
     if fig is None and ax is None:
-        fig = plt.figure(figsize=(10, 10))
+        fig = plt.figure(figsize=figsize)
         fig.canvas.toolbar_visible = False
         fig.canvas.header_visible = False
         fig.canvas.footer_visible = False
@@ -77,44 +131,116 @@ def plot_isosurface(verts, faces, x_min, x_max, y_min, y_max, θ_min, θ_max, dx
         ax.set_xlim(x_min, x_max)
         ax.set_ylim(y_min, y_max)
         ax.set_zlim(θ_min, θ_max)
-        ax.set_xlabel("x")
-        ax.set_ylabel("y")
-        ax.set_zlabel("θ")
-    ax.plot_trisurf(x_min + verts[:, 1] * dxy, y_max - verts[:, 0] * dxy, faces, θ_min + verts[:, 2] * dθ, alpha=alpha, label=label)
+        ax.set_xlabel("$x$")
+        ax.set_ylabel("$y$")
+        ax.set_zlabel("$θ$")
+    ax.plot_trisurf(x_min + verts[:, 1] * dxy, y_max - verts[:, 0] * dxy, faces, θ_min + verts[:, 2] * dθ, alpha=alpha,
+                    label=label)
     return fig, ax
-    
+
+# def plot_isosurface_SO3(verts, faces, α_min, α_max, β_min, β_max, φ_min, φ_max, dα, dβ, dφ, alpha=0.5, label=None,
+#                     figsize=(10, 10), fig=None, ax=None):
+#     """Plot the isosurface given by `verts` and `faces`."""
+#     if fig is None and ax is None:
+#         fig = plt.figure(figsize=figsize)
+#         fig.canvas.toolbar_visible = False
+#         fig.canvas.header_visible = False
+#         fig.canvas.footer_visible = False
+#         fig.canvas.resizable = False
+#         ax = fig.add_subplot(111, projection="3d")
+#         ax.set_xlim(β_max, β_min)
+#         ax.set_ylim(α_min, α_max)
+#         ax.set_zlim(φ_min, φ_max)
+#         ax.set_xlabel("$β$")
+#         ax.set_ylabel("$α$")
+#         ax.set_zlabel("$φ$")
+#     ax.plot_trisurf(β_min + verts[:, 0] * dβ, α_min + verts[:, 1] * dα, faces, φ_min + verts[:, 2] * dφ, alpha=alpha,
+#                     label=label)
+#     return fig, ax
 
 def overwrite_default(passed_value, default_value):
+    """
+    Overwrite the value of some parameter if the user has not passed that
+    parameter.
+    """
     if passed_value is None: # User did not pass any value
         passed_value = default_value
     return default_value
 
 def plot_vector_field(vector_field, xs, ys, color="red", figsize=(10, 10), fig=None, ax=None, x_min=None, x_max=None, 
                       y_min=None, y_max=None):
+    """Streamplot of `vector_field`."""
     if fig is None and ax is None:
         fig, ax = plt.subplots(figsize=figsize)
         ax.set_xlabel("$x$")
         ax.set_ylabel("$y$")
         x_min = overwrite_default(x_min, xs[0, 0])
         x_max = overwrite_default(x_max, xs[-1, -1])
-        y_min = overwrite_default(y_min, ys[-1, -1])
-        y_max = overwrite_default(y_max, ys[0, 0])
+        y_min = overwrite_default(y_min, ys[0, 0])
+        y_max = overwrite_default(y_max, ys[-1, -1])
         ax.set_xlim(x_min, x_max)
         ax.set_ylim(y_min, y_max)
-    ax.streamplot(np.flip(xs, axis=0), np.flip(ys, axis=0), np.flip(vector_field[..., 0], axis=0), np.flip(vector_field[..., 1], axis=0), color=color)
+
+    xs_aligned = align_to_standard_array_axis_scalar_field(xs)
+    ys_aligned = align_to_standard_array_axis_scalar_field(ys)
+    vector_field_aligned = align_to_standard_array_axis_vector_field(vector_field)
+
+    ax.streamplot(np.flip(xs_aligned, axis=0), np.flip(ys_aligned, axis=0),
+                  np.flip(vector_field_aligned[..., 0], axis=0), 
+                  np.flip(vector_field_aligned[..., 1], axis=0), color=color)
     return fig, ax
 
 def plot_scalar_field(scalar_field, xs, ys, levels=None, figsize=(12, 10), fig=None, ax=None, x_min=None, x_max=None, 
                       y_min=None, y_max=None):
+    """Plot two-dimensional `scalar_field` using coloured in contours."""
     if fig is None and ax is None:
         fig, ax = plt.subplots(figsize=figsize)
         ax.set_xlabel("$x$")
         ax.set_ylabel("$y$")
         x_min = overwrite_default(x_min, xs[0, 0])
         x_max = overwrite_default(x_max, xs[-1, -1])
-        y_min = overwrite_default(y_min, ys[-1, -1])
-        y_max = overwrite_default(y_max, ys[0, 0])
+        y_min = overwrite_default(y_min, ys[0, 0])
+        y_max = overwrite_default(y_max, ys[-1, -1])
         ax.set_xlim(x_min, x_max)
         ax.set_ylim(y_min, y_max)
-    contour = ax.contourf(xs, ys, scalar_field, levels=levels)
+
+    xs_aligned = align_to_standard_array_axis_scalar_field(xs)
+    ys_aligned = align_to_standard_array_axis_scalar_field(ys)
+    scalar_field_aligned = align_to_standard_array_axis_scalar_field(scalar_field)
+
+    contour = ax.contourf(xs_aligned, ys_aligned, scalar_field_aligned, levels=levels)
+    return fig, ax, contour
+
+def plot_vector_field_SO3(vector_field, αs, βs, color="red", figsize=(10, 10), fig=None, ax=None, α_min=None,
+                          α_max=None, β_min=None, β_max=None):
+    """Streamplot of `vector_field`."""
+    if fig is None and ax is None:
+        fig, ax = plt.subplots(figsize=figsize)
+        ax.set_xlabel("$β$")
+        ax.set_ylabel("$α$")
+        α_min = overwrite_default(α_min, αs[0, 0])
+        α_max = overwrite_default(α_max, αs[-1, -1])
+        β_min = overwrite_default(β_min, βs[0, 0])
+        β_max = overwrite_default(β_max, βs[-1, -1])
+        ax.set_xlim(β_max, β_min)
+        ax.set_ylim(α_min, α_max)
+
+    ax.streamplot(βs, αs, vector_field[..., 1], vector_field[..., 0], color=color)
+    return fig, ax
+
+def plot_scalar_field_SO3(scalar_field, αs, βs, levels=None, figsize=(12, 10), fig=None, ax=None, α_min=None,
+                          α_max=None, β_min=None, β_max=None):
+    """Plot two-dimensional `scalar_field` using coloured in contours."""
+    if fig is None and ax is None:
+        fig, ax = plt.subplots(figsize=figsize)
+        ax.set_xlabel("$β$")
+        ax.set_ylabel("$α$")
+        α_min = overwrite_default(α_min, αs[0, 0])
+        α_max = overwrite_default(α_max, αs[-1, -1])
+        β_min = overwrite_default(β_min, βs[0, 0])
+        β_max = overwrite_default(β_max, βs[-1, -1])
+        ax.set_xlim(β_max, β_min)
+        ax.set_ylim(α_min, α_max)
+
+    contour = ax.contourf(βs, αs, scalar_field, levels=levels)
     return fig, ax, contour
