070a7c65898a364fb4aac2d8eee10aa7ccf8fcb1
Finn Sherry <f.m.sherry@tue.nl>
2024-05-12 15:37:26 +0200

    start of gauge frame implementation.

---
+++ dsfilter/SE2/gauge/__init__.py
@@ -0,0 +1,20 @@
+"""
+    SE2
+    ==
+
+    Apply Diffusion-Shock filtering on SE(2) using gauge frames.
+
+    Provides the following "top level" submodule:
+      1. filter: apply the Diffusion-Shock filter to an image on SE(2).
+
+    Additionally, we have the following "internal" submodules
+      1. derivatives: compute various derivatives of functions on SE(2).
+      2. switches: compute the quantities that switch between diffusion and
+      shock and between erosion and dilation.
+"""
+
+# Access entire backend
+# import dsfilter.SE2.gauge.filter
+# import dsfilter.SE2.gauge.derivatives
+# import dsfilter.SE2.gauge.switches
+import dsfilter.SE2.gauge.frame
---
+++ dsfilter/SE2/gauge/frame.py
@@ -0,0 +1,192 @@
+"""
+    frame
+    =====
+
+    Compute the gauge frames fitted to data on SE(2), using:
+      1. `compute_gauge_frame`: 
+"""
+
+import taichi as ti
+import numpy as np
+from dsfilter.SE2.utils import scalar_trilinear_interpolate
+from dsfilter.SE2.regularisers import (
+    convolve_with_kernel_x_dir,
+    convolve_with_kernel_y_dir,
+    convolve_with_kernel_θ_dir
+)
+
+
+A = ti.Matrix(np.diag((-1, 2, 1)), dt=ti.f32)
+print(A)
+
+@ti.kernel
+def test_eig(
+    mat: ti.types.matrix(m=3, n=3, dtype=ti.f32)
+) -> ti.types.matrix(m=3, n=3, dtype=ti.f32):
+    eigenvalues, eigenvectors = ti.sym_eig(A)
+    return eigenvectors
+
+@ti.kernel
+def test_eig_2(
+    mat: ti.types.matrix(m=3, n=3, dtype=ti.f32)
+) -> ti.types.vector(n=3, dtype=ti.f32):
+    eigenvalues, eigenvectors = ti.sym_eig(A)
+    return eigenvalues
+
+test_eig_2(A)
+
+@ti.func
+def compute_gauge_frame(
+    u: ti.template(),
+    dxy: ti.f32,
+    dθ: ti.f32,
+    θs: ti.template(),
+    k_int_s: ti.template(),
+    radius_int_s: ti.i32,
+    k_int_o: ti.template(),
+    radius_int_o: ti.i32,
+    k_ext_s: ti.template(),
+    radius_ext_s: ti.i32,
+    k_ext_o: ti.template(),
+    radius_ext_o: ti.i32,
+    ξ: ti.f32,
+    A1_u: ti.template(),
+    A2_u: ti.template(),
+    A3_u: ti.template(),
+    H: ti.template(),
+    A: ti.template(),
+    B1: ti.template(),
+    B2: ti.template(),
+    B3: ti.template(),
+    convolution_storage_1: ti.template(),
+    convolution_storage_2: ti.template()
+):
+    """
+    @taichi.func
+
+    Compute an approximation of the Hessian matrix H^i_j `u` = A_j A_i `u`, the
+    components of the Lie-Cartan 0 connection Hessian with respect to the left
+    invariant frame.
+
+    Args:
+      Static:
+        `u`: ti.field(dtype=[float], shape=[Nx, Ny, Nθ]) which we want to
+          differentiate.
+        `dxy`: step size in x and y direction, taking values greater than 0.
+        `dθ`: step size in orientational direction, taking values greater than
+          0.
+        `θs`: angle coordinate at each grid point.
+        `k_int_s`: ti.field(dtype=[float], shape=2*`radius_s`+1) Gaussian kernel
+          used for spatial regularisation.
+        `radius_int_s`: radius at which kernel `k_s` is truncated, taking
+          integer values greater than 0.
+        `k_int_o`: ti.field(dtype=[float], shape=2*`radius_o`+1) Gaussian kernel
+          used for orientational regularisation.
+        `radius_int_o`: radius at which kernel `k_ext` is truncated, taking
+          integer values greater than 0.
+        `k_ext_s`: ti.field(dtype=[float], shape=2*`radius_s`+1) Gaussian kernel
+          used for spatial regularisation.
+        `radius_ext_s`: radius at which kernel `k_s` is truncated, taking
+          integer values greater than 0.
+        `k_ext_o`: ti.field(dtype=[float], shape=2*`radius_o`+1) Gaussian kernel
+          used for orientational regularisation.
+        `radius_ext_o`: radius at which kernel `k_ext` is truncated, taking
+          integer values greater than 0.
+      Mutated:
+        `laplacian_u`: ti.field(dtype=[float], shape=[Nx, Ny, Nθ]) laplacian of
+          u, which is updated in place.
+    """
+    # First regularise with Gaussian convolution.
+    convolve_with_kernel_x_dir(u, k_int_s, radius_int_s, convolution_storage_1)
+    convolve_with_kernel_y_dir(convolution_storage_1, k_int_s, radius_int_s, convolution_storage_2)
+    convolve_with_kernel_θ_dir(convolution_storage_2, k_int_o, radius_int_o, u)
+    # Then compute Hessian matrix.
+    compute_Hessian_matrix(u, dxy, dθ, θs, A1_u, A2_u, A3_u, H)
+    M = ti.Matrix(np.diag((1/ξ, 1/ξ, 1.)), dt=ti.f32)
+    for I in ti.grouped(A):
+        A[I] = H[I].transpose() * (M**2) * H[I]
+    _, H = ti.sym_eig(A)
+    for I in ti.grouped(B1):
+        c1 = H[I][0, 2]
+        sign = ti.math.sign(c1)
+        c1 *= sign
+        c2 = H[I][1, 2] * sign
+        c3 = H[I][2, 2] * sign
+        χ = ti.math.atan2(c2, c1)
+        cosχ = ti.math.cos(χ)
+        sinχ = ti.math.sin(χ)
+        ν = ti.math.atan2(c3, ti.math.sqrt(c1**2 + c2**2))
+        cosν = ti.math.cos(ν)
+        sinν = ti.math.sin(ν)
+        B1[I][0] = c1
+        B1[I][1] = c2
+        B1[I][2] = c3
+        B2[I][0] = -sinχ / ξ
+        B2[I][1] = cosχ / ξ
+        B2[I][2] = 0
+        B3[I][0] = -cosχ * sinν / ξ
+        B3[I][1] = -sinχ * sinν / ξ
+        B3[I][2] = cosν
+
+
+
+@ti.func
+def compute_Hessian_matrix(
+    u: ti.template(),
+    dxy: ti.f32,
+    dθ: ti.f32,
+    θs: ti.template(),
+    A1_u: ti.template(),
+    A2_u: ti.template(),
+    A3_u: ti.template(),
+    H: ti.template()
+):
+    """
+    @taichi.func
+
+    Compute an approximation of the Hessian matrix H^i_j `u` = A_j A_i `u`, the
+    components of the Lie-Cartan 0 connection Hessian with respect to the left
+    invariant frame.
+
+    Args:
+      Static:
+        `u`: ti.field(dtype=[float], shape=[Nx, Ny, Nθ]) which we want to
+          differentiate.
+        `dxy`: step size in x and y direction, taking values greater than 0.
+        `dθ`: step size in orientational direction, taking values greater than
+          0.
+        `θs`: angle coordinate at each grid point.
+      Mutated:
+        `A*_u`: ti.field(dtype=[float], shape=[Nx, Ny, Nθ]) of derivatives, which
+          are updated in place.
+        `H`: ti.Matrix.field(m=3, n=3, dtype=[float], shape=[Nx, Ny, Nθ]) field
+          of Lie-Cartan 0 connection Hessian matrices with respect to the left
+          invariant frame, which is updated in place.
+    """
+    I_A3 = ti.Vector([0.0,  0.0, 1.0], dt=ti.f32)/2 # We do 2 first order central differences.
+    # First order derivatives.
+    for I in ti.grouped(u):
+        θ = θs[I]
+        cos = ti.math.cos(θ)
+        sin = ti.math.sin(θ)
+        I_A1 = ti.Vector([cos, sin, 0.0], dt=ti.f32)/2
+        I_A2 = ti.Vector([-sin, cos, 0.0], dt=ti.f32)/2
+        A1_u[I] = (scalar_trilinear_interpolate(u, I + I_A1) - scalar_trilinear_interpolate(u, I - I_A1)) / dxy
+        A2_u[I] = (scalar_trilinear_interpolate(u, I + I_A2) - scalar_trilinear_interpolate(u, I - I_A2)) / dxy
+        A3_u[I] = (scalar_trilinear_interpolate(u, I + I_A3) - scalar_trilinear_interpolate(u, I - I_A3)) / dθ
+    # Second order mixed derivatives.
+    for I in ti.grouped(H):
+        θ = θs[I]
+        cos = ti.math.cos(θ)
+        sin = ti.math.sin(θ)
+        I_A1 = ti.Vector([cos, sin, 0.0], dt=ti.f32)/2
+        I_A2 = ti.Vector([-sin, cos, 0.0], dt=ti.f32)/2
+        H[I][0, 0] = (scalar_trilinear_interpolate(A1_u, I + I_A1) - scalar_trilinear_interpolate(A1_u, I - I_A1)) / dxy # A11
+        H[I][0, 1] = (scalar_trilinear_interpolate(A1_u, I + I_A2) - scalar_trilinear_interpolate(A1_u, I - I_A2)) / dxy # A21
+        H[I][0, 2] = (scalar_trilinear_interpolate(A1_u, I + I_A3) - scalar_trilinear_interpolate(A1_u, I - I_A3)) / dθ  # A31
+        H[I][1, 0] = (scalar_trilinear_interpolate(A2_u, I + I_A1) - scalar_trilinear_interpolate(A2_u, I - I_A1)) / dxy # A12
+        H[I][1, 1] = (scalar_trilinear_interpolate(A2_u, I + I_A2) - scalar_trilinear_interpolate(A2_u, I - I_A2)) / dxy # A22
+        H[I][1, 2] = (scalar_trilinear_interpolate(A2_u, I + I_A3) - scalar_trilinear_interpolate(A2_u, I - I_A3)) / dθ  # A32
+        H[I][2, 0] = (scalar_trilinear_interpolate(A3_u, I + I_A1) - scalar_trilinear_interpolate(A3_u, I - I_A1)) / dxy # A13
+        H[I][2, 1] = (scalar_trilinear_interpolate(A3_u, I + I_A2) - scalar_trilinear_interpolate(A3_u, I - I_A2)) / dxy # A23
+        H[I][2, 2] = (scalar_trilinear_interpolate(A3_u, I + I_A3) - scalar_trilinear_interpolate(A3_u, I - I_A3)) / dθ  # A33
